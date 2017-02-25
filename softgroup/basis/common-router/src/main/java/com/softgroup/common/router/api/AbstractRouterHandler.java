f01017062d393c1f839d1c90434f8c5e8b5794e6
Yura Kuchevskiy <yurakuchevskiy@mail.ru>
2017-02-25 18:22:26 +0200

    changed AbstractRouterHandler and AbstractRequestHandler

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRouterHandler.java
@@ -2,8 +2,26 @@ package com.softgroup.common.router.api;
 
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractRouterHandler<T extends Handler> implements RouterHandler {
+
+	@Autowired
+	private List<T> handlers;
+
+	private Map<String, T> handlerMap = new HashMap<>();
+
+	@PostConstruct
+	public void init() {
+		for (T handler : handlers) {
+			handlerMap.put(handler.getName(), handler);
+		}
+	}
 
 	@Override
 	public String getRouteKey(Request<?> msg) {
@@ -12,7 +30,7 @@ public abstract class AbstractRouterHandler<T extends Handler> implements Router
 
 	@Override
 	public Response<?> handle(Request<?> msg) {
-		return null;
+		return handlerMap.get(getRouteKey(msg)).handle(msg);
 	}
 
 }
