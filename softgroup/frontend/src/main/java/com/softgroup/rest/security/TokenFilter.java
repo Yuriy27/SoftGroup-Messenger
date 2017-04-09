ee0889483694493cc5e28318026d0454c38af31a
Yuriy27 <yurakuchevskiy@mail.ru>
2017-04-09 21:22:01 +0300

    git conflict slf4j import resolved

---
+++ softgroup/frontend/src/main/java/com/softgroup/rest/security/TokenFilter.java
@@ -0,0 +1,32 @@
+package com.softgroup.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by yuriy27 on 08.04.17.
+ */
+public class TokenFilter extends UsernamePasswordAuthenticationFilter {
+
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+}
